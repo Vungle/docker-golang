cfdb341fc5796567e4bd3893488b254e8101b903
Steve Jiang <steve.jiang@vungle.com>
2017-08-25 09:58:18 -0700

    fix(deploy): fixes deploy script so that it handles patch version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -ex
 ver=`cat .version`
+
+# Ensure that when .version file is missing patch version part, amend the
+# default '0' patch version.
+IFS='.' read -r -a verparts <<< "${ver}"
+if [ "${#verparts[@]}" -eq "2" ]; then
+  ver="${ver}.0"
+fi
+
 minorver=`echo ${ver} | awk -F \. '{ print $1"."$2 }'`
 majorver=`echo ${ver} | awk -F \. '{ print $1 }'`
 make build PROJECT_IMAGE=vungle/golang:${ver}
